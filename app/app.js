98584773235c12c1a3efeac2165da165de32d125
Thomas J. Shavor <thomasshavor@gmail.com>
2016-08-31 14:56:25 -0500

    initial commit

--- app/app.js
+++ app/app.js
@@ -1,34 +1,68 @@
 "use strict";
 
-$scope.items = [
-  {
-    id: 0,
-    task: "mow the lawn",
-    isCompleted: false,
-    dueDate: "12/5/17",
-    assignedTo: "Greg",
-    location: "Joe's house",
-    urgency: "low",
-    dependencies: "sunshine, clippers, hat, water, headphones"
-  },
-  {
-    id: 1,
-    task: "grade quizzes",
-    isCompleted: false,
-    dueDate: "12/5/15",
-    assignedTo: "Christina",
-    location: "NSS",
-    urgency: "high",
-    dependencies: "wifi, tissues, vodka"
-  },
-  {
-    id: 2,
-    task: "take a nap",
-    isCompleted: false,
-    dueDate: "5/21/16",
-    assignedTo: "Joe",
-    location: "Porch of lakefront cabin",
-    urgency: "medium",
-    dependencies: "hammock, silence"
-  }
-];
+// create a new angular module.  2 arguments: [] holds external modukes to load into this one...
+var app= angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function ($scope){
+    $scope.navItems= [
+        {name: "Logout"},
+        {name: "All Items"},
+        {name: "New Items"}
+        ];
+
+});
+
+app.controller("TodoCtrl", function ($scope){
+    $scope.items = [
+      {
+        id: 0,
+        task: "mow the lawn",
+        isCompleted: false,
+        dueDate: "12/5/17",
+        assignedTo: "Greg",
+        location: "Joe's house",
+        urgency: "low",
+        dependencies: "sunshine, clippers, hat, water, headphones"
+      },
+      {
+        id: 1,
+        task: "grade quizzes",
+        isCompleted: false,
+        dueDate: "12/5/15",
+        assignedTo: "Christina",
+        location: "NSS",
+        urgency: "high",
+        dependencies: "wifi, tissues, vodka"
+      },
+      {
+        id: 2,
+        task: "take a nap",
+        isCompleted: false,
+        dueDate: "5/21/16",
+        assignedTo: "Joe",
+        location: "Porch of lakefront cabin",
+        urgency: "medium",
+        dependencies: "hammock, silence"
+      }
+    ];
+        $scope.newTask = {};
+        $scope.showListView = true;
+
+        $scope.newItem = function(){
+        $scope.showListView = false;
+        };
+
+        $scope.allItem = function(){
+        $scope.showListView = true;
+    };
+
+        $scope.addNewItem= function(){
+        $scope.newTask.isCompleted = false;
+        $scope.newTask.id = $scope.items.length;
+        $scope.items.push($scope.newTask);
+        $scope.newTask= {};
+
+    };
+
+});
+
